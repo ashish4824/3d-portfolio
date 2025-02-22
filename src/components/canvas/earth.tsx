5d4b88a7f9288ad0ffa0b57463e0b879450da25b
ashish4824 <ashish7860635522@gmail.com>
2025-02-22 15:33:36 +0530

    t

--- src/components/canvas/earth.tsx
+++ src/components/canvas/earth.tsx
@@ -7,8 +7,8 @@ import CanvasLoader from "../loader";
 // Earth
 const Earth = () => {
   // import earth scene
-  const earth = useGLTF("./planet/scene.gltf");
-
+  // const earth = useGLTF("./planet/scene.gltf");
+  const earth = useGLTF("/planet/scene.gltf");
   return (
     <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
   );
